8dd0de8765fb53e664e79d4899e2ad3f34308963
Niklas Menge <n.menge@ra-micro.de>
2023-06-12 09:29:47 +0200

    pseudo merge from frontend

--- Frontend/rasandbox/src/App.vue
+++ Frontend/rasandbox/src/App.vue
@@ -1,29 +1,82 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div>
+    <h1 class="main-title">Sandbox Project</h1>
+    <div class="main-input">
+      <input type="text" class="SBinput" v-model="inputText" />
+      <button @click="submitGame(inputText)" class="SBbutton">Submit</button>
+    </div>
+    <div class="gameBox" v-for="game in gamesList" :key="game.name">
+      {{ game.name }}
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "./components/HelloWorld.vue";
+import Vue from "vue";
+import Games from "@/response.json";
 
-@Component({
-  components: {
-    HelloWorld,
+export default Vue.extend({
+  name: "App",
+  components: {},
+  data() {
+    return {
+      inputText: "",
+      gamesList: [],
+    };
   },
-})
-export default class App extends Vue {}
+  methods: {
+    requestGames() {
+      this.gamesList = Games.response.games;
+    },
+    submitGame(name) {
+      this.gamesList.push({ name: name });
+    },
+  },
+  beforeMount() {
+    this.requestGames();
+  },
+});
 </script>
 
 <style>
-#app {
+html {
+  background-image: linear-gradient(cyan, hotpink);
+}
+.main-title {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.SBinput {
+  border: none;
+  border-radius: 10px;
+  background-color: lightgrey;
+  padding-left: 5px;
+  height: 25px;
+}
+.SBbutton {
+  height: 27px;
+  margin-left: 5px;
+  border: none;
+  border-radius: 10px;
+  background-color: lightgrey;
+}
+.main-input {
+  display: flex;
+  justify-content: center;
+}
+.gameBox {
+  border: 2px solid black;
+  height: 10vh;
+  width: 90vw;
+  margin: 0 auto;
+  margin-top: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 20px;
 }
 </style>
