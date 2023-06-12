8bf77b083c628d2a66269b030d1374b3a478a4d2
Anton Lehniger <a.lehniger@ra-micro.de>
2023-06-12 13:10:23 +0200

    modal works

--- Frontend/rasandbox/src/components/User-modal.vue
+++ Frontend/rasandbox/src/components/User-modal.vue
@@ -2,7 +2,7 @@
   <div class="modal">
     <div class="modal-background"></div>
     <div class="modal-content">
-      <div class="box">test</div>
+      <div class="box">Hallo Robert!</div>
     </div>
     <button
       class="modal-close is-large"
