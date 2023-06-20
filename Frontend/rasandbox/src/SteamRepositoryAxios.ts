643e06df90a2eb58ff6d98081e7e776d323605e8
Anton Lehniger <a.lehniger@ra-micro.de>
2023-06-20 15:21:36 +0200

    keine ahnung mehr rober hilfe

--- Frontend/rasandbox/src/SteamRepositoryAxios.ts
+++ Frontend/rasandbox/src/SteamRepositoryAxios.ts
@@ -1,7 +1,11 @@
+import { AxiosRepository } from "./AxiosRepository";
 import Games from "./response.json";
 import { SteamRepository } from "@/SteamRepository";
 
-export class SteamRepositoryAxios implements SteamRepository {
+export class SteamRepositoryAxios
+  extends AxiosRepository
+  implements SteamRepository
+{
   public loadGames() {
     return Promise.resolve(Games);
   }
