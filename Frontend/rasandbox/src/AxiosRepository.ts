221be6b5eedade6c0c740cedc3fb216018c32e8c
Anton Lehniger <a.lehniger@ra-micro.de>
2023-06-26 10:54:17 +0200

    updated axiosrepo

--- Frontend/rasandbox/src/AxiosRepository.ts
+++ Frontend/rasandbox/src/AxiosRepository.ts
@@ -1,11 +1,17 @@
-/* eslint-disable */
-import { AxiosRequestConfig, AxiosInstance } from "axios";
+import { AxiosRequestConfig, AxiosInstance, AxiosResponse } from "axios";
+import { mapResponseSimple } from "@/shared/api/jsonapi/responseMappers";
+
+type ResponseMapper<R> = (response: AxiosResponse) => R;
 
 export class AxiosRepository {
   constructor(protected axios: AxiosInstance) {}
 
-  protected async sendGet<T>(url: string, config?: AxiosRequestConfig) {
-    return await this.axios.get(url, config);
+  protected async sendGet<T>(
+    url: string,
+    config?: AxiosRequestConfig,
+    mappingFn: ResponseMapper<T> = (r) => this.transformResponse(r)
+  ) {
+    return mappingFn(await this.axios.get(url, config));
   }
 
   protected async sendHtmlGet(url: string, config?: AxiosRequestConfig) {
@@ -14,34 +20,42 @@ export class AxiosRepository {
 
   protected async sendDelete<T = void>(
     url: string,
-    config?: AxiosRequestConfig
+    config?: AxiosRequestConfig,
+    mappingFn: ResponseMapper<T> = (r) => this.transformResponse(r)
   ): Promise<T extends void ? void : T> {
     const response = await this.axios.delete(url, config);
     // @ts-ignore
-    return response ? response : Promise.resolve();
+    return response ? mappingFn(response) : Promise.resolve();
   }
 
   protected async sendPost<T>(
     url: string,
     data?: any,
-    config?: AxiosRequestConfig
+    config?: AxiosRequestConfig,
+    mappingFn: ResponseMapper<T> = (r) => this.transformResponse(r)
   ) {
-    return await this.axios.post(url, data, config);
+    return mappingFn(await this.axios.post(url, data, config));
   }
 
   protected async sendPut<T>(
     url: string,
     data?: any,
-    config?: AxiosRequestConfig
+    config?: AxiosRequestConfig,
+    mappingFn: ResponseMapper<T> = (r) => this.transformResponse(r)
   ) {
-    return await this.axios.put(url, data, config);
+    return mappingFn(await this.axios.put(url, data, config));
   }
 
   protected async sendPatch<T>(
     url: string,
     data?: any,
-    config?: AxiosRequestConfig
+    config?: AxiosRequestConfig,
+    mappingFn: ResponseMapper<T> = (r) => this.transformResponse(r)
   ) {
-    return await this.axios.patch(url, data, config);
+    return mappingFn(await this.axios.patch(url, data, config));
+  }
+
+  protected transformResponse(response: AxiosResponse): any {
+    return mapResponseSimple(response, (data) => data);
   }
 }
