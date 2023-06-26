a9d235bfc8a021369ecf8fcc448b778603e03193
sleepyRias <46777993+sleepyRias@users.noreply.github.com>
2023-06-26 14:15:18 +0200

    added Put

--- sandBox2/Controllers/GameController.cs
+++ sandBox2/Controllers/GameController.cs
@@ -25,5 +25,28 @@ namespace backend.Controllers
             var games = _dataProvider.Games.ToList();
             return games;
         }
+
+        [HttpPut("Games/{id}")]
+        public IActionResult UpdateGame(int id, [FromBody] Game updatedGame)
+        {
+            var game = _dataProvider.Games.FirstOrDefault(g => g.Id == id);
+            if (game == null)
+            {
+                return NotFound(); // Falls das Spiel mit der angegebenen ID nicht gefunden wurde
+            }
+
+            // Aktualisiere die Eigenschaften des Spiels mit den Werten aus updatedGame
+            game.Name = updatedGame.Name;
+            game.Genre = updatedGame.Genre;
+            game.Company = updatedGame.Company;
+            game.Price = updatedGame.Price;
+            game.ReleaseDate = updatedGame.ReleaseDate;
+
+            // Speichere die Änderungen in der Datenbank
+            _dataProvider.SaveChanges();
+
+            return Ok(); // Erfolgreiche Aktualisierung
+        }
+
     }
 }
