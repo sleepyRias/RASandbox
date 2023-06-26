28f289c3cf7b2381c6b83d6a784c26666cae106d
Anton Lehniger <a.lehniger@ra-micro.de>
2023-06-26 14:15:17 +0200

    getting user now works

--- Frontend/rasandbox/src/components/User-modal.vue
+++ Frontend/rasandbox/src/components/User-modal.vue
@@ -4,38 +4,52 @@
     <div class="modal-content">
       <div class="box">
         <div class="columns">
-          <div class="column">
-            <figure class="image is-128x128">
-              <img src="https://bulma.io/images/placeholders/128x128.png" />
-            </figure>
+          <div v-if="name !== ''">
+            <div class="column">
+              <figure class="image is-128x128">
+                <img src="https://bulma.io/images/placeholders/128x128.png" />
+              </figure>
+            </div>
+            <div class="column is-multiline">
+              <span>Username: {{ username }}</span>
+              <button class="button" @click="changeUsername = true">
+                Change Username
+              </button>
+              <input v-if="changeUsername" v-model="username" class="input" />
+              <button
+                v-if="changeUsername"
+                @click="storeChanges"
+                class="button is-success"
+              >
+                Submit
+              </button>
+            </div>
+            <div class="column">
+              <span>Name: {{ name }}</span>
+              <button @click="changeName = true" class="button">
+                Change Name
+              </button>
+              <input v-if="changeName" v-model="name" class="input" />
+              <button
+                v-if="changeName"
+                @click="storeChanges"
+                class="button is-success"
+              >
+                Submit
+              </button>
+            </div>
           </div>
-          <div class="column is-multiline">
-            <span>Username: {{ username }}</span>
-            <button class="button" @click="changeUsername = true">
-              Change Username
-            </button>
-            <input v-if="changeUsername" v-model="username" class="input" />
-            <button
-              v-if="changeUsername"
-              @click="storeChanges"
-              class="button is-success"
-            >
-              Submit
-            </button>
-          </div>
-          <div class="column">
-            <span>Name: {{ name }}</span>
-            <button @click="changeName = true" class="button">
-              Change Name
-            </button>
-            <input v-if="changeName" v-model="name" class="input" />
-            <button
-              v-if="changeName"
-              @click="storeChanges"
-              class="button is-success"
-            >
-              Submit
-            </button>
+          <div v-else-if="name === ''">
+            <div class="column">
+              <span>Loaduser:</span>
+              <input v-model="username" class="input" />
+              <button
+                class="button is-primary"
+                @click="loadUserWithKey(username)"
+              >
+                Load
+              </button>
+            </div>
           </div>
         </div>
       </div>
@@ -45,6 +59,7 @@
 </template>
 <script lang="ts">
 import Vue from "vue";
+import { User } from "../User";
 
 export default Vue.extend({
   name: "UserModal",
@@ -62,6 +77,11 @@ export default Vue.extend({
         name: this.name,
       };
       localStorage.setItem(this.username, JSON.stringify(user));
+    },
+    loadUserWithKey(key: string) {
+      var user = {} as User;
+      user = JSON.parse(localStorage.getItem(key));
+      this.name = user.name;
     },
   },
 });
