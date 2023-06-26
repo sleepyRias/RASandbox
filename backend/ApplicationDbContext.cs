0022bd143faf8c735796294024f9d758cedb5c59
Niklas Menge <n.menge@ra-micro.de>
2023-06-26 10:52:49 +0200

    .

---
+++ backend/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace backend
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        { }
+
+        public DbSet<Game> Game { get; set; }
+    }
+}
