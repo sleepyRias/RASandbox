d97d8d3c0b98b106a7b5ffe666465f7053cece1a
Anton Lehniger <a.lehniger@ra-micro.de>
2023-06-26 11:27:37 +0200

    lint works again

--- Frontend/rasandbox/shared/api/JsonApiResponse.ts
+++ Frontend/rasandbox/shared/api/JsonApiResponse.ts
@@ -1,6 +1,7 @@
 import { ArrayElement } from "../ArrayElement";
 import { JsonApiResource } from "./JsonApiResource";
 export interface JsonApiResponse<T, M = undefined, RM = undefined> {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   data: T extends any[]
     ? JsonApiResource<ArrayElement<T>, M>[]
     : JsonApiResource<T, M>;
